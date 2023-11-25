bdbb3ed9168390903d750770536c9b23bf7fc773
ceoxcellentsoft <ceo@xcellentsoft.com>
2023-11-25 14:11:46 -0500

    my first java class

---
+++ src/test/java/com/serenitydojo/HelloWorldWriterTest.java
@@ -0,0 +1,11 @@
+package com.serenitydojo;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldWriterTest {
+    @Test
+    public void shouldWriteHelloWorldToTheConsole(){
+        System.out.println("Hello world!");
+    }
+}
+
